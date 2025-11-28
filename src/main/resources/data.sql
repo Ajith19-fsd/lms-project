cab5b2ac41504ebf2ecde6aedba7406f49b3398e
Ajith <ajithraja1904@gmail.com>
2025-11-28 12:35:27 +0530

    Prepare LMS backend for Render deployment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,13 @@
-INSERT INTO roles (name) VALUES ('ADMIN')
+-- Insert default roles only if they do not already exist
+
+INSERT INTO roles (name)
+VALUES ('ADMIN')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO roles (name) VALUES ('INSTRUCTOR')
+INSERT INTO roles (name)
+VALUES ('INSTRUCTOR')
 ON CONFLICT DO NOTHING;
 
-INSERT INTO roles (name) VALUES ('STUDENT')
+INSERT INTO roles (name)
+VALUES ('STUDENT')
 ON CONFLICT DO NOTHING;
