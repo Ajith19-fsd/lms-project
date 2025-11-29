b3479cc5bf1f48081d588759be7792cc33dda67e
Ajith <ajithraja1904@gmail.com>
2025-11-29 12:38:33 +0530

    Prepare LMS backend for Render deployment

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,13 +16,13 @@ ON CONFLICT (name) DO NOTHING;
 -- =========================
 -- Default Admin User
 -- =========================
--- Password is BCrypt encoded: ajith123
+-- Password is BCrypt encoded: admin123
 INSERT INTO users (full_name, username, email, password, role_id, created_at, updated_at)
 VALUES (
-    'Ajith',
-    'Admin Ajith',
-    'ajith@example.com',
-    '$2a$10$7EqJtq98hPqEX7fNZaFWoOa6mXbRoeG1pWJcEYoq0r5r14Xim6V5W',
+    'Admin',
+    'Admin User',
+    'admin@example.com',
+    '$2a$10$yXGslF6x9kY1W7ZB/lkRAuE3GmW1x9u3YvYcSxF3N0Q3F5kZ9K8fS', -- bcrypt for admin123
     (SELECT id FROM roles WHERE name='ADMIN'),
     NOW(),
     NOW()
